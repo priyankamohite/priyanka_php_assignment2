924976b43928b52924267b5f886163c15b6ae130
webonise <webonise@webonise-desktop.(none)>
2012-09-14 13:00:09 +0530

    created a connection

--- php_assignment_pdo.php
+++ php_assignment_pdo.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "Hello";
+/*** mysql hostname ***/
+$hostname = 'localhost';
+
+/*** mysql username ***/
+$username = 'root';
+
+/*** mysql password ***/
+$password = 'borntoowin';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+    /*** show successful message ***/
+    echo 'Connected to database';
+    }
+catch(PDOException $e)
+    {
+    /*** show error message when fails to connect ***/
+    echo $e->getMessage();
+    }
 
 ?>
