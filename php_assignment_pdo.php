9f3510142b5b8d3773d1257a287e492bbbe12b65
webonise <webonise@webonise-desktop.(none)>
2012-09-14 14:17:04 +0530

    written functions to make and close connection

--- php_assignment_pdo.php
+++ php_assignment_pdo.php
@@ -9,18 +9,24 @@ $username = 'root';
 /*** mysql password ***/
 $password = 'borntoowin';
 
-try {
-    $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
-    /*** show successful message ***/
-    echo 'Connected to database<br/>';
-    /*** closed the database connection ***/
-    $dbh=null;
-    echo 'connection closed';
-    }
-catch(PDOException $e)
-    {
-    /*** show error message when fails to connect ***/
-    echo $e->getMessage();
-    }
+function makeConnection()
+{
+	try {
+	    $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+	    /*** show successful message ***/
+	    echo 'Connected to database<br/>';
+	    }
+	catch(PDOException $e)
+	    {
+	    /*** show error message when fails to connect ***/
+	    echo $e->getMessage();
+	    }
+}
 
+function closeConnection()
+{
+  	    /*** closed the database connection ***/
+	    $dbh=null;
+	    echo 'connection closed';
+}
 ?>
