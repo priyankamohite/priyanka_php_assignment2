bb9041626dfc6002fecc7cba6c954db0f7486923
webonise <webonise@webonise-desktop.(none)>
2012-09-14 16:38:58 +0530

    fetch associate and num mode done

--- php_assignment_pdo.php
+++ php_assignment_pdo.php
@@ -1,21 +1,24 @@
 <?php
 
-/*** mysql hostname ***/
-$hostname = 'localhost';
-
-/*** mysql username ***/
-$username = 'root';
-
-/*** mysql password ***/
-$password = 'borntoowin';
 
 function makeConnection()
 {
+	/*** mysql hostname ***/
+	$hostname = 'localhost';
+
+	/*** mysql username ***/
+	$username = 'root';
+
+	/*** mysql password ***/
+	$password = 'borntoowin';
+
 	try {
-	    $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+	    $dbh = new PDO("mysql:host=$hostname;dbname=mydb", $username, $password);
 	    /*** show successful message ***/
-	    echo 'Connected to database<br/>';
-	    }
+	    echo 'Connected to database<br/><br/>';
+	return $dbh;
+            }
+
 	catch(PDOException $e)
 	    {
 	    /*** show error message when fails to connect ***/
@@ -25,8 +28,35 @@ function makeConnection()
 
 function closeConnection()
 {
-  	    /*** closed the database connection ***/
+   
+	/*** closed the database connection ***/
 	    $dbh=null;
-	    echo 'connection closed';
+	    echo '<br/><br/>Connection closed';
 }
+
+function fetch($dbh,$mode)
+{
+
+	$sql="select * from user";
+	$stmt=$dbh->query($sql);
+	$result = $stmt->fetch($mode);
+	foreach($result as $key=>$val)
+	{
+	 echo $key.' - '.$val.'<br />';
+	}
+}
+
+$dbh= makeConnection();
+
+echo "FETCH_ASSOC mode:<br/>";
+$mode=PDO::FETCH_ASSOC;
+fetch($dbh,$mode);
+
+echo "<br/>FETCH_NUM mode:<br/>";
+$mode=PDO::FETCH_NUM;
+fetch($dbh,$mode);
+
+closeConnection();
+
+
 ?>
